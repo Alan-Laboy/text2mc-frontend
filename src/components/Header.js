49f3dc1d7cb7ac044abeb77b2cd63958c5a328a3
Ryan Turner <ryanturner02@protonmail.com>
2024-10-17 15:42:25 -0400

    Retrieve header image in styles.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,11 @@
 import "./styles.css"
 
 export default function Header(){
-
     return(
-
-        <div className="site-header" style={{
-            backgroundImage: `url(${require('./images/title-bg.png')})`,
-        }}>
+        <div className="site-header">
             <div className="title-container">
                 <h1 className="Title">text2MC</h1>
             </div>
-            
-            
         </div>
-
     )
-
 }
-
