6f7007ede007b606f160003bc2c14724156ad9b9
Ryan Turner <ryanturner02@protonmail.com>
2024-10-17 15:32:40 -0400

    Update navbar hrefs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,12 +10,12 @@ export default function NavigationBar(){
             <Navbar>
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="nav">
-                    <Nav.Link className="nav-text" href="/">Home</Nav.Link>
-                    <Nav.Link className="nav-text" href="#intro">Introduction</Nav.Link>
-                    <Nav.Link className="nav-text" href="#block2vec">Block2Vec</Nav.Link>
-                    <Nav.Link className="nav-text" href="#model">AI Model</Nav.Link>
-                    <Nav.Link className="nav-text" hreff="#faq">FAQ</Nav.Link>
-                    <Nav.Link className="nav-text" href="#link">Links</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#">Home</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#intro">Introduction</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#block2vec">Block2Vec</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#model">AI Model</Nav.Link>
+                    <Nav.Link className="nav-text" hreff="/#faq">FAQ</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#link">Links</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
             </Navbar>
