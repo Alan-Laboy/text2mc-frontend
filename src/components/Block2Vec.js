ee448e263ac4b441b46b83e548fd1c66d796babf
Ryan Turner <ryanturner02@protonmail.com>
2024-10-17 15:47:03 -0400

    Link navbar hrefs to the component ids

--- src/components/Block2Vec.js
+++ src/components/Block2Vec.js
@@ -1,6 +1,6 @@
 export default function Block2Vec() {
     return (
-        <div>
+        <div id="block2vec">
             <h1>Block2Vec</h1>
             <p>Veniam aute tempor non mollit laboris non velit. Tempor officia anim fugiat labore ad culpa ipsum eiusmod dolor reprehenderit dolor tempor ipsum aliquip. Voluptate adipisicing adipisicing cupidatat ut tempor ad consectetur sunt. Sit amet enim ex irure.</p>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,9 +13,9 @@ export default function NavigationBar(){
                     <Nav.Link className="nav-text" href="/#">Home</Nav.Link>
                     <Nav.Link className="nav-text" href="/#intro">Introduction</Nav.Link>
                     <Nav.Link className="nav-text" href="/#block2vec">Block2Vec</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#model">AI Model</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#ai-model">AI Model</Nav.Link>
                     <Nav.Link className="nav-text" href="/#faq">FAQ</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#link">Links</Nav.Link>
+                    <Nav.Link className="nav-text" href="/#links">Links</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
             </Navbar>
