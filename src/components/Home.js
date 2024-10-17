5512a2fdf5d164847758972dc14b6a7a64981580
Ryan Turner <ryanturner02@protonmail.com>
2024-10-17 15:40:06 -0400

    Add Links section component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Block2Vec from './Block2Vec';
 import FAQ from './FAQ';
 import Header from './Header';
 import Introduction from './Introduction';
+import Links from './Links';
 import NavigationBar from './Nav';
 
 function Home() {
@@ -15,6 +16,7 @@ function Home() {
         <Block2Vec />
         <AIModel />
         <FAQ />
+        <Links />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,8 @@
+export default function Links() {
+    return (
+        <div id="links">
+            <h1>Links</h1>
+            <p>Non magna ex nisi labore labore nostrud esse ipsum ea et ut elit.</p>
+        </div>
+    );
+}
