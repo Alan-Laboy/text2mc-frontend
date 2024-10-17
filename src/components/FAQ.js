e6ba0c1b1a40ebbed22d98ff8c3a1109b324a0cb
Ryan Turner <ryanturner02@protonmail.com>
2024-10-17 15:37:33 -0400

    Fix FAQ href

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,7 +1,7 @@
 export default function FAQ() {
     return (
-        <div>
-            <h1 id="faq">Frequently Asked Questions</h1>
+        <div id="faq">
+            <h1>Frequently Asked Questions</h1>
             <p>Ad ex pariatur duis sint duis mollit dolor eiusmod adipisicing ex. Mollit adipisicing fugiat sit labore Lorem. Laboris aliqua qui incididunt duis aliqua nulla aute.</p>
             <p>Id sit mollit eiusmod ea occaecat nulla fugiat quis consectetur reprehenderit ipsum aute sunt officia. Eu voluptate id occaecat sunt id reprehenderit velit est do. Nisi ad culpa voluptate mollit amet esse elit labore. Consectetur non exercitation veniam anim pariatur amet aliquip mollit minim ex veniam pariatur. Quis aute consequat exercitation qui consectetur eu nostrud amet in ullamco id. Non officia dolor ipsum aliqua proident veniam exercitation dolore. Veniam magna enim ullamco officia excepteur. Duis cillum veniam adipisicing aliquip minim cupidatat ut velit. Adipisicing ad Lorem aliquip et reprehenderit ex duis nostrud nostrud nulla. Irure enim officia tempor incididunt aliqua. Et ex do sint qui in est.</p>
         </div>
