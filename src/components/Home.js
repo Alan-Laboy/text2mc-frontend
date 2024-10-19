081fb01ff7d7757e5b887b8e19bf2db8691acdc3
Ryan Turner <ryanturner02@protonmail.com>
2024-10-19 12:03:58 -0400

    Add Bootstrap grid to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,34 @@
-
-import AIModel from './AIModel';
-import Block2Vec from './Block2Vec';
-import FAQ from './FAQ';
-import Header from './Header';
-import Introduction from './Introduction';
-import Links from './Links';
-import NavigationBar from './Nav';
+import AIModel from "./AIModel";
+import Block2Vec from "./Block2Vec";
+import FAQ from "./FAQ";
+import Header from "./Header";
+import Introduction from "./Introduction";
+import Links from "./Links";
+import NavigationBar from "./Nav";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Home() {
   return (
-    <div className='containter'>
-        <Header />
-        <NavigationBar />
-        <Introduction />
-        <Block2Vec />
-        <AIModel />
-        <FAQ />
-        <Links />
+    <div className="containter">
+      <Container fluid>
+        <Row>
+          <Header />
+          <NavigationBar />          
+        </Row>
+        <Row>
+          <Col />
+          <Col>
+            <Introduction />
+            <Block2Vec />
+            <AIModel />
+            <FAQ />
+            <Links />
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </div>
   );
 }
