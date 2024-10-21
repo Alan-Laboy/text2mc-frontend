b0cf77b94b38325f8f537fed490f394745182f7d
Ryan Turner <ryanturner02@protonmail.com>
2024-10-21 14:34:10 -0400

    Add margin under Links div

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,6 +1,6 @@
 export default function Links() {
     return (
-        <div id="links">
+        <div className="mb-5" id="links">
             <h1>Links</h1>
             <p>Non magna ex nisi labore labore nostrud esse ipsum ea et ut elit.</p>
         </div>
