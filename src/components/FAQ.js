8456b8e43ece31115aa3dcd2acf1dfce0110ef2d
Ryan Turner <ryanturner02@protonmail.com>
2024-10-21 14:28:55 -0400

    Update FAQ placeholder text

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,16 +6,16 @@ export default function FAQ() {
             <h1>Frequently Asked Questions</h1>
             <Accordion>
               <Accordion.Item eventKey="0">
-                <Accordion.Header>Question 1</Accordion.Header>
-                <Accordion.Body>Duis deserunt ex esse amet nulla voluptate est et.</Accordion.Body>
+                <Accordion.Header>How did you retrieve the data for the AI Model?</Accordion.Header>
+                <Accordion.Body>We got maps from PlanetMinecraft and did computer magic.</Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="1">
-                <Accordion.Header>Question 2</Accordion.Header>
-                <Accordion.Body>Duis deserunt ex esse amet nulla voluptate est et.</Accordion.Body>
+                <Accordion.Header>What was the inspiration for text2mc?</Accordion.Header>
+                <Accordion.Body>Ryan was here and put placeholder text here.</Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="2">
-                <Accordion.Header>Question 3</Accordion.Header>
-                <Accordion.Body>Duis deserunt ex esse amet nulla voluptate est et.</Accordion.Body>
+                <Accordion.Header>How did you do this project?</Accordion.Header>
+                <Accordion.Body>Ryan was not here but there is placeholder text already here!</Accordion.Body>
               </Accordion.Item>
             </Accordion>
         </div>
