8750ae89be36d26e5e5b9e62bd1d414173641725
Ryan Turner <ryanturner02@protonmail.com>
2024-10-21 14:49:49 -0400

    Add repos and World-GAN links

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Links() {
     return (
-        <div className="mb-5" id="links">
+        <div className="mb-5 flex-col" id="links">
             <h1>Links</h1>
-            <p>Non magna ex nisi labore labore nostrud esse ipsum ea et ut elit.</p>
+            <div>
+                <Link to="https://github.com/shauncomino/text2mc-dataprocessor">text2mc dataprocessor</Link>
+            </div>
+            <div>
+                <Link to="https://github.com/Alan-Laboy/text2mc-frontend">text2mc frontend</Link>
+            </div>
+            <div>
+                <Link to="https://github.com/Mawiszus/World-GAN">World-GAN</Link>
+            </div>
         </div>
     );
 }
