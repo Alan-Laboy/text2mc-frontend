599fd8c99b8354ffa09868edf1fbb1cf32d49be6
Ryan Turner <ryanturner02@protonmail.com>
2024-10-22 12:57:42 -0400

    Update header and navbar colors

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,15 +4,15 @@ import Nav from "react-bootstrap/Nav"
 export default function NavigationBar(){
     return(
         <div className="nav-container d-flex justify-content-center mb-3">
-            <Navbar>
+            <Navbar data-bs-theme="dark">
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="nav">
-                    <Nav.Link className="nav-text" href="/#">Home</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#intro">Introduction</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#block2vec">Block2Vec</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#ai-model">AI Model</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#faq">FAQ</Nav.Link>
-                    <Nav.Link className="nav-text" href="/#links">Links</Nav.Link>
+                    <Nav.Link href="/#">Home</Nav.Link>
+                    <Nav.Link href="/#intro">Introduction</Nav.Link>
+                    <Nav.Link href="/#block2vec">Block2Vec</Nav.Link>
+                    <Nav.Link href="/#ai-model">AI Model</Nav.Link>
+                    <Nav.Link href="/#faq">FAQ</Nav.Link>
+                    <Nav.Link href="/#links">Links</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
             </Navbar>
