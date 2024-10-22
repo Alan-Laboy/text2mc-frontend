dc896f8cc9a15efa680a3018a84ae71c7bb1ae79
Ryan Turner <ryanturner02@protonmail.com>
2024-10-22 14:06:23 -0400

    Update navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,16 +5,19 @@ import Nav from "react-bootstrap/Nav";
 export default function NavigationBar() {
   return (
     <>
-      <Navbar className="nav-container mb-3" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/#">text2mc</Navbar.Brand>
-          <Nav classname="me-auto">
-            <Nav.Link href="/#intro">Introduction</Nav.Link>
-            <Nav.Link href="/#block2vec">Block2Vec</Nav.Link>
-            <Nav.Link href="/#ai-model">AI Model</Nav.Link>
-            <Nav.Link href="/#faq">FAQ</Nav.Link>
-            <Nav.Link href="/#links">Links</Nav.Link>
-          </Nav>
+      <Navbar className="nav-container mb-3" data-bs-theme="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/#">Home</Nav.Link>
+              <Nav.Link href="/#intro">Introduction</Nav.Link>
+              <Nav.Link href="/#block2vec">Block2Vec</Nav.Link>
+              <Nav.Link href="/#ai-model">AI Model</Nav.Link>
+              <Nav.Link href="/#faq">FAQ</Nav.Link>
+              <Nav.Link href="/#links">Links</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
