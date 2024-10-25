4cc68671ab301c783a6ebc2836706cb3c63d147d
Ryan Turner <ryanturner02@protonmail.com>
2024-10-24 22:29:24 -0400

    Update link text and add World-GAN Research Paper links

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,13 +6,16 @@ export default function Links() {
       <h1>Links</h1>
       <ul>
         <li>
-        <Link to="https://github.com/shauncomino/text2mc-dataprocessor">text2mc dataprocessor</Link>
+        <Link to="https://github.com/shauncomino/text2mc-dataprocessor">text2mc Data Processor: GitHub Repo</Link>
         </li>
         <li>
-            <Link to="https://github.com/Alan-Laboy/text2mc-frontend">text2mc frontend</Link>
+            <Link to="https://github.com/Alan-Laboy/text2mc-frontend">text2mc Frontend: GitHub Repo</Link>
         </li>
         <li>
-            <Link to="https://github.com/Mawiszus/World-GAN">World-GAN</Link>
+          <Link to="https://arxiv.org/abs/2106.10155">World-GAN: Research Paper</Link>
+        </li>
+        <li>
+          <Link to="https://github.com/Mawiszus/World-GAN">World-GAN: GitHub Repo</Link>
         </li>
         <li>
             <Link to="https://www.planetminecraft.com/">Planet Minecraft</Link>
