98f10f6e74cd5a60630f149a0b81f5cddbdfb231
Ryan Turner <ryanturner02@protonmail.com>
2024-10-24 22:02:28 -0400

    Crop epoch images

--- src/components/AIModel.js
+++ src/components/AIModel.js
@@ -1,13 +1,13 @@
 import Image from "react-bootstrap/Image";
-import Epoch15 from "./images/epoch_15.gif";
-import Epoch83 from "./images/epoch_83.gif";
+import Epoch15 from "./images/epoch-15-cropped.gif";
+import Epoch83 from "./images/epoch-83-cropped.gif";
 
 export default function AIModel() {
     return (
         <div className="mb-5" id="ai-model">
             <h1>AI Model</h1>
             <p>Sunt occaecat eiusmod do officia tempor deserunt. Do laborum magna elit quis magna aute. Irure duis sunt elit ex ex cupidatat laboris.</p>
-            <div className="d-flex justify-content-evenly">
+            <div className="d-flex justify-content-around">
                 <Image src={Epoch15} alt="Epoch 15" className="epoch-image" />
                 <Image src={Epoch83} alt="Epoch 83" className="epoch-image" />
             </div>
