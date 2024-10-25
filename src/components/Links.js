b750da48bcb08418691019f49e0583b32d7e95f7
Ryan Turner <ryanturner02@protonmail.com>
2024-10-24 21:45:12 -0400

    Put links in an unordered list

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
 export default function Links() {
-    return (
-        <div className="mb-5 flex-col" id="links">
-            <h1>Links</h1>
-            <div>
-                <Link to="https://github.com/shauncomino/text2mc-dataprocessor">text2mc dataprocessor</Link>
-            </div>
-            <div>
-                <Link to="https://github.com/Alan-Laboy/text2mc-frontend">text2mc frontend</Link>
-            </div>
-            <div>
-                <Link to="https://github.com/Mawiszus/World-GAN">World-GAN</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mb-5" id="links">
+      <h1>Links</h1>
+      <ul>
+        <li>
+        <Link to="https://github.com/shauncomino/text2mc-dataprocessor">text2mc dataprocessor</Link>
+        </li>
+        <li>
+            <Link to="https://github.com/Alan-Laboy/text2mc-frontend">text2mc frontend</Link>
+        </li>
+        <li>
+            <Link to="https://github.com/Mawiszus/World-GAN">World-GAN</Link>
+        </li>
+        <li>
+            <Link to="https://www.planetminecraft.com/">Planet Minecraft</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
