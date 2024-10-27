ea92dfa184603e2f69a1447f51b583ab67146dc6
Ryan Turner <ryanturner02@protonmail.com>
2024-10-27 16:20:29 -0400

    Make embeddings image responsive

--- src/components/Block2Vec.js
+++ src/components/Block2Vec.js
@@ -7,7 +7,7 @@ export default function Block2Vec() {
         <div className="mb-5" id="block2vec">
             <h1>Block2Vec</h1>
             <p>Veniam aute tempor non mollit laboris non velit. Tempor officia anim fugiat labore ad culpa ipsum eiusmod dolor reprehenderit dolor tempor ipsum aliquip. Voluptate adipisicing adipisicing cupidatat ut tempor ad consectetur sunt. Sit amet enim ex irure.</p>
-            <div className="d-flex justify-content-center">
+            <div className="text-center">
                 <Image src={ Embeddings1 } alt="Graph of Minecraft blocks showing the semantic relationship between each block" className="embeddings-image" />
             </div>
             <p className="text-center">Graph of Minecraft blocks showing the semantic relationship between each block</p>
