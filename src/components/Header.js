4847c18875108973c2d0c00cd1217241122999b9
Ryan Turner <ryanturner02@protonmail.com>
2024-10-27 14:40:44 -0400

    Update header
    Able to select text in the intro and b2v sections now

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,8 @@ import "./styles.css"
 
 export default function Header(){
     return(
-        <div className="site-header">
-            <div className="title-container">
-                <h1 className="Title">text2mc</h1>
-            </div>
+        <div className="site-header d-flex justify-content-center align-items-center">
+            <h1 className="title display-1 fw-bolder py-4 px-5 rounded-pill">text2mc</h1>
         </div>
     )
 }
