c0da29ee8b65358aaf04d5a5a81d1e30a9db2a7f
Ryan Turner <ryanturner02@protonmail.com>
2024-10-27 16:37:34 -0400

    YAGNI: Remove unused embeddings image import

--- src/components/Block2Vec.js
+++ src/components/Block2Vec.js
@@ -1,6 +1,5 @@
 import Image from "react-bootstrap/Image";
-import Embeddings1 from "../images/embeddings/embeddings_scatterplot_2d.png";
-import Embeddings2 from "../images/embeddings/embeddings_scatterplot_2d_1.png";
+import Embeddings from "../images/embeddings/embeddings_scatterplot_2d.png";
 
 export default function Block2Vec() {
     return (
@@ -8,7 +7,7 @@ export default function Block2Vec() {
             <h1>Block2Vec</h1>
             <p>Veniam aute tempor non mollit laboris non velit. Tempor officia anim fugiat labore ad culpa ipsum eiusmod dolor reprehenderit dolor tempor ipsum aliquip. Voluptate adipisicing adipisicing cupidatat ut tempor ad consectetur sunt. Sit amet enim ex irure.</p>
             <div className="text-center">
-                <Image src={ Embeddings1 } alt="Graph of Minecraft blocks showing the semantic relationship between each block" className="embeddings-image" />
+                <Image src={ Embeddings } alt="Graph of Minecraft blocks showing the semantic relationship between each block" className="embeddings-image" />
             </div>
             <p className="text-center">Graph of Minecraft blocks showing the semantic relationship between each block</p>
         </div>
