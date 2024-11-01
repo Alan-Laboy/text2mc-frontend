16e7a761ec0f0f366a0ac7bdc464ec5b0f97d1d6
Ryan Turner <ryanturner02@protonmail.com>
2024-10-31 21:24:15 -0400

    Replace header background with text shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "./styles.css"
 export default function Header(){
     return(
         <div className="header d-flex justify-content-center align-items-center">
-            <h1 className="header-title display-1 fw-bolder py-4 px-5 rounded-pill">text2mc</h1>
+            <h1 className="header-title display-1 fw-bolder">text2mc</h1>
         </div>
     )
 }
