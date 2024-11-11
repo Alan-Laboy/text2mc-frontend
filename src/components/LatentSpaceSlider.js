ab3f2cb9fb8740ab63744e965e7112a64bb3b01a
Ryan Turner <ryanturner02@protonmail.com>
2024-11-10 22:16:47 -0500

    Add image slider functionality

--- src/components/LatentSpaceSlider.js
+++ src/components/LatentSpaceSlider.js
@@ -1,20 +1,31 @@
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
+import Image from "react-bootstrap/Image";
+import TestImage from "../images/temporary_image.png";
 
 export default function LatentBuildSlider() {
   const [interpolationValue, setInterpolationValue] = useState(0);
+  const [interpolationImageSrc, setInterpolationImageSrc] = useState("");
+  const interpolationImages = [TestImage, TestImage, TestImage, TestImage, TestImage];
+
+  const updateInterpolation = (event) => {
+    const sliderValue = event.target.value;
+    setInterpolationValue(sliderValue);
+    setInterpolationImageSrc(interpolationImages[sliderValue]);
+  };
 
   return (
-    <Form>
-      <Form.Label>Latent Space Interpolation</Form.Label>
-      <Form.Range
-        min="0"
-        max="4"
-        value={interpolationValue}
-        onChange={(event) => {
-          setInterpolationValue(event.target.value);
-        }}
-      />
-    </Form>
+    <div>
+      <Image src={interpolationImageSrc} alt="Test image" fluid />
+      <Form>
+        <Form.Label>Latent Space Interpolation</Form.Label>
+        <Form.Range
+          min="0"
+          max="4"
+          value={interpolationValue}
+          onChange={updateInterpolation}
+        />
+      </Form>
+    </div>
   );
 }
