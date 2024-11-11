5a5be0f3b96c4756c554384ac86b7d4c4825c296
Ryan Turner <ryanturner02@protonmail.com>
2024-11-10 21:59:03 -0500

    Add updatable range slider

--- src/components/LatentSpaceSlider.js
+++ src/components/LatentSpaceSlider.js
@@ -1,10 +1,20 @@
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 export default function LatentBuildSlider() {
-  return ( 
-    <div>
+  const [interpolationValue, setInterpolationValue] = useState(0);
+
+  return (
+    <Form>
       <Form.Label>Latent Space Interpolation</Form.Label>
-      <Form.Range min={0} max={5} />
-    </div>
+      <Form.Range
+        min="0"
+        max="4"
+        value={interpolationValue}
+        onChange={(event) => {
+          setInterpolationValue(event.target.value);
+        }}
+      />
+    </Form>
   );
 }
